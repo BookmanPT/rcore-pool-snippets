59d960f50b2771f770559187aa30ba76921bb385
BookmanPT <64754185+BookmanPT@users.noreply.github.com>
2024-03-07 14:36:36 +0000

    pt-PT translation
    Locale pt-PT with full translation into Portuguese from Portugal

---
+++ locales/portuguese/pt.lua
@@ -0,0 +1,54 @@
+Text = {
+    BACK = "Regressar",
+    HIT = "Dar tacada",
+
+    BALL_IN_HAND = "Bola na mão",
+    BALL_IN_HAND_BACK = "Regressar",
+
+    AIM_LEFT = "Apontar esquerda",
+    AIM_RIGHT = "Apontar direita",
+    AIM_SLOWER = "Apontar lentamente",
+
+    POOL = 'Snooker',
+    POOL_GAME = 'Jogo Snooker',
+    POOL_SUBMENU = 'Escolher configuração de bolas',
+
+    TYPE_8_BALL = '8-ball',
+    TYPE_STRAIGHT = 'Straight pool',
+
+    POOL_SETUP = 'Configurar: ',
+
+    HINT_SETUP = 'Configurar mesa',
+    HINT_TAKE_CUE = 'Agarre o taco de snooker',
+    HINT_RETURN_CUE = 'Devolva o taco de snooker',
+    HINT_HINT_TAKE_CUE = 'Para jogar Snooker, pegue o taco de snooker no suporte de tacos',
+    HINT_PLAY = 'Jogar',
+
+    NOT_ENOUGH_MONEY = 'Não tem dinheiro suficiente €%s para jogar nesta mesa.',
+
+    BALL_IN_HAND_LEFT = 'Esquerda',
+    BALL_IN_HAND_RIGHT = 'Direita',
+    BALL_IN_HAND_UP = 'Cima',
+    BALL_IN_HAND_DOWN = 'Baixo',
+    BALL_POCKETED = '%s meteste a bola no buraco',
+    BALL_IN_HAND_NOTIFY = 'Tem a bola branca na mão',
+    
+    BALL_LABELS = {
+        [-1] = 'Branca',
+        [1] = 'Lisa 1',
+        [2] = 'Lisa 2',
+        [3] = 'Lisa 3',
+        [4] = 'Lisa 4',
+        [5] = 'Lisa 5',
+        [6] = 'Lisa 6',
+        [7] = 'Lisa 7',
+        [8] = 'Preta 8',
+        [9] = 'Listrada 9',
+        [10] = 'Listrada 10',
+        [11] = 'Listrada 11',
+        [12] = 'Listrada 12',
+        [13] = 'Listrada 13',
+        [14] = 'Listrada 14',
+        [15] = 'Listrada 15',
+    }
+},
